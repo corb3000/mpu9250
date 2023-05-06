46b11e104e9fab235828a577b67f35947e41eab2
Mark Corridge <corb3000@gmail.com>
2023-05-06 16:08:51 -0700

    output raw and Mag

--- mpu9250/mpu9250.py
+++ mpu9250/mpu9250.py
@@ -11,7 +11,7 @@ from .imusensor.filters import madgwick
 import rclpy
 from rclpy.node import Node
 
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, MagneticField
 
 from math import sin, cos, radians
 import tf_transformations
@@ -23,8 +23,12 @@ class MyPythonNode(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('frequency', 30),
-                ('frame_id', 'imu_link'),
+                ('pub_mag', True),
+                ('pub_raw', True),
+                ('pub_data', True),
+                ('use_mag', True),
+                ('frequency', 100),
+                ('frame_id', 'imu'),
                 ('i2c_address', 0x68),
                 ('i2c_port', 1),
                 ('acceleration_scale', [1.0, 1.0, 1.0]),
@@ -50,12 +54,14 @@ class MyPythonNode(Node):
         self.imu.MagBias = np.asarray(self.get_parameter('magnetometer_bias')._value)
         self.imu.Magtransform = np.reshape(np.asarray(self.get_parameter('magnetometer_transform')._value),(3,3))
 
-        self.publisher_imu_values_ = self.create_publisher(Imu, "/imu", 10)
-        self.timer_publish_imu_values_ = self.create_timer(
-            1.0/self.get_parameter('frequency')._value, self.publish_imu_values)
+        self.publisher_imu_raw_ = self.create_publisher(Imu, "/imu/data_raw", 10)
+        self.publisher_imu_data_ = self.create_publisher(Imu, "/imu/data", 10)
+        self.publisher_imu_mag_ = self.create_publisher(MagneticField, "/imu/mag", 10)
+
+        self.timer_publish_imu_values_ = self.create_timer(1.0/self.get_parameter('frequency')._value, self.publish_imu_values)
 
         self.sensorfusion = kalman.Kalman()
-        #self.sensorfusion = madgwick.Madgwick(0.5)
+        # self.sensorfusion = madgwick.Madgwick(0.5)
         self.imu.begin()
         self.imu.readSensor()    
         self.imu.computeOrientation()
@@ -66,48 +72,59 @@ class MyPythonNode(Node):
         self.lastTime = self.get_clock().now()
 
     def publish_imu_values(self):
+        msg_data = Imu()
+        msg_mag = MagneticField()
+
         self.imu.readSensor()
-        #self.imu.computeOrientation()
-        msg = Imu()
+        time_stamp = self.get_clock().now().to_msg()       
         deltaTime = (self.get_clock().now() - self.lastTime).nanoseconds * 10e9
         self.lastTime = self.get_clock().now()
-        #yaw = self.imu.yaw
-        #pitch = self.imu.pitch
-        #roll = self.imu.roll
-        #computeAndUpdateRollPitchYaw
-        self.sensorfusion.computeAndUpdateRollPitchYaw(\
-            self.imu.AccelVals[0], self.imu.AccelVals[1], self.imu.AccelVals[2],\
-            self.imu.GyroVals[0], self.imu.GyroVals[1], self.imu.GyroVals[2],\
-	        self.imu.MagVals[0], self.imu.MagVals[1], self.imu.MagVals[2], deltaTime)
-        yaw = self.sensorfusion.yaw
-        pitch = self.sensorfusion.pitch
-        roll = self.sensorfusion.roll
 
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = self.get_parameter('frame_id')._value
+        msg_data.header.stamp = time_stamp
+        msg_data.header.frame_id = self.get_parameter('frame_id')._value
         # Direct measurements
-        msg.linear_acceleration_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
-        msg.linear_acceleration.x = self.imu.AccelVals[0]
-        msg.linear_acceleration.y = self.imu.AccelVals[1]
-        msg.linear_acceleration.z = self.imu.AccelVals[2]
-        msg.angular_velocity_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
-        msg.angular_velocity.x = (self.imu.GyroVals[0]) #TODO this is acceleration not velocity (?!)
-        msg.angular_velocity.y = (self.imu.GyroVals[1]) #TODO this is acceleration not velocity (?!)
-        msg.angular_velocity.z = (self.imu.GyroVals[2]) #TODO this is acceleration not velocity (?!)
-        #msg.angular_velocity.x = 0.0 #TODO this is acceleration not velocity (?!)
-        #msg.angular_velocity.y = 0.0 #TODO this is acceleration not velocity (?!)
-        #msg.angular_velocity.z = 0.0 #TODO this is acceleration not velocity (?!)
-        # Calculate euler angles, convert to quaternion and store in message
-        msg.orientation_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
-        # Convert to quaternion
-        quat = tf_transformations.quaternion_from_euler(
-            radians(roll), radians(pitch), radians(yaw))
-        msg.orientation.x = quat[0]
-        msg.orientation.y = quat[1]
-        msg.orientation.z = quat[2]
-        msg.orientation.w = quat[3]
-        self.publisher_imu_values_.publish(msg)
-        print("roll: {:4.2f} \tpitch : {:4.2f} \tyaw : {:4.2f}".format(self.sensorfusion.roll, self.sensorfusion.pitch, self.sensorfusion.yaw))
+        msg_data.linear_acceleration_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
+        msg_data.linear_acceleration.x = self.imu.AccelVals[0]
+        msg_data.linear_acceleration.y = self.imu.AccelVals[1]
+        msg_data.linear_acceleration.z = self.imu.AccelVals[2]
+        msg_data.angular_velocity_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
+        msg_data.angular_velocity.x = (self.imu.GyroVals[0]) 
+        msg_data.angular_velocity.y = (self.imu.GyroVals[1]) 
+        msg_data.angular_velocity.z = (self.imu.GyroVals[2])
+
+        if self.get_parameter('pub_raw')._value: 
+            self.publisher_imu_raw_.publish(msg_data)
+
+        if self.get_parameter('pub_data')._value:        
+            self.sensorfusion.computeAndUpdateRollPitchYaw(\
+                self.imu.AccelVals[0], self.imu.AccelVals[1], self.imu.AccelVals[2],\
+                self.imu.GyroVals[0], self.imu.GyroVals[1], self.imu.GyroVals[2],\
+                self.imu.MagVals[0], self.imu.MagVals[1], self.imu.MagVals[2], deltaTime)
+            yaw = self.sensorfusion.yaw
+            pitch = self.sensorfusion.pitch
+            roll = self.sensorfusion.roll
+
+            # Calculate euler angles, convert to quaternion and store in message
+            msg_data.orientation_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
+            # Convert to quaternion
+            quat = tf_transformations.quaternion_from_euler(
+                radians(roll), radians(pitch), radians(yaw))
+            msg_data.orientation.x = quat[0]
+            msg_data.orientation.y = quat[1]
+            msg_data.orientation.z = quat[2]
+            msg_data.orientation.w = quat[3]
+            self.publisher_imu_data_.publish(msg_data)
+        # print("roll: {:4.2f} \tpitch : {:4.2f} \tyaw : {:4.2f}".format(self.sensorfusion.roll, self.sensorfusion.pitch, self.sensorfusion.yaw))
+
+        if self.get_parameter('pub_mag')._value: 
+            msg_mag.header.stamp = time_stamp
+            msg_mag.header.frame_id = self.get_parameter('frame_id')._value            
+            msg_mag.magnetic_field.x = (self.imu.MagVals[0])
+            msg_mag.magnetic_field.y = (self.imu.MagVals[1])
+            msg_mag.magnetic_field.z = (self.imu.MagVals[2])
+            msg_mag.magnetic_field_covariance = [0.0025, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0025]
+            self.publisher_imu_mag_.publish(msg_mag)
+
 
 def main(args=None):
     rclpy.init(args=args)
