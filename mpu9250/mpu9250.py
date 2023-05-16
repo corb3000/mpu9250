ca594ff0f547ab9fcc2354ca620513b5e88e13b5
Mark Corridge <corb3000@gmail.com>
2023-05-15 18:46:01 -0700

    Clean up publishers

--- mpu9250/mpu9250.py
+++ mpu9250/mpu9250.py
@@ -24,16 +24,15 @@ class MyPythonNode(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('pub_mag', True),
-                ('pub_raw', True),
-                ('pub_data', True),
-                ('use_mag', True),
+                ('imu_pub_mag', True),
+                ('imu_pub_raw', True),
+                ('imu_pub_data', True),
                 ('cal_mag', False),
-                ('cal_acc', False),
-                ('frequency', 100),
-                ('frame_id', 'imu'),
-                ('i2c_address', 0x68),
-                ('i2c_port', 1),
+                ('cal_accel', False),
+                ('imu_sample_frequency', 100),
+                ('imu_frame_id', 'imu_link'),
+                ('imu_i2c_address', 0x68),
+                ('imu_i2c_port', 1),
                 ('acceleration_scale', [1.0, 1.0, 1.0]),
                 ('acceleration_bias', [0.0, 0.0, 0.0]),
                 ('gyro_bias', [0.0, 0.0, 0.0]),
@@ -43,15 +42,17 @@ class MyPythonNode(Node):
                     1.0, 0.0, 0.0,
                     0.0, 1.0, 0.0,
                     0.0, 0.0, 1.0]),
-                ('output_transform', [
+                ('imu_output_transform', [
+
+                    
                     0.0, 1.0, 0.0,
                     1.0, 0.0, 0.0,
                     0.0, 0.0, -1.0]),
             ]
         )
 
-        address = self.get_parameter('i2c_address')._value
-        bus = smbus.SMBus(self.get_parameter('i2c_port')._value)
+        address = self.get_parameter('imu_i2c_address')._value
+        bus = smbus.SMBus(self.get_parameter('imu_i2c_port')._value)
         self.imu = MPU9250.MPU9250(bus, address)
 
         self.imu.Accels = np.asarray(self.get_parameter('acceleration_scale')._value)
@@ -60,15 +61,22 @@ class MyPythonNode(Node):
         self.imu.Mags = np.asarray(self.get_parameter('magnetometer_scale')._value)
         self.imu.MagBias = np.asarray(self.get_parameter('magnetometer_bias')._value)
         self.imu.Magtransform = np.reshape(np.asarray(self.get_parameter('magnetometer_transform')._value),(3,3))
-        self.imu.OutputTransform = np.reshape(np.asarray(self.get_parameter('output_transform')._value),(3,3))
+        self.imu.OutputTransform = np.reshape(np.asarray(self.get_parameter('imu_output_transform')._value),(3,3))
+        self.pub_mag_ = self.get_parameter('imu_pub_raw')._value
+        self.pub_data_ = self.get_parameter('imu_pub_data')._value
+        self.pub_raw_ = self.get_parameter('imu_pub_raw')._value
+        self.sample_frequency_ = self.get_parameter('imu_sample_frequency')._value
 
-        self.publisher_imu_raw_ = self.create_publisher(Imu, "/imu/data_raw", 10)
-        self.publisher_imu_data_ = self.create_publisher(Imu, "/imu/data/imu", 10)
-        self.publisher_imu_mag_ = self.create_publisher(MagneticField, "/imu/mag", 10)
+        if self.pub_raw_:
+            self.publisher_imu_raw_ = self.create_publisher(Imu, "/imu/data_raw", 10)
+        if self.pub_data_:
+            self.publisher_imu_data_ = self.create_publisher(Imu, "/imu/data/imu", 10)
+        if self.pub_mag_:
+            self.publisher_imu_mag_ = self.create_publisher(MagneticField, "/imu/mag", 10)
 
-        print("Node started")
+        print("IMU Node started")
 
-        if self.get_parameter('cal_acc')._value:
+        if self.get_parameter('cal_accel')._value:
             self.imu.begin()
             self.imu.caliberateAccelerometer() # calibrate ACC
 
@@ -76,7 +84,7 @@ class MyPythonNode(Node):
             self.imu.begin()
             self.imu.caliberateMagPrecise() # calibrate mag
 
-        if self.get_parameter('cal_acc')._value or self.get_parameter('cal_mag')._value:
+        if self.get_parameter('cal_accel')._value or self.get_parameter('cal_mag')._value:
             self.imu.saveCalibDataToFile('/home/bigshark/dev_ws/imu_cal.json')
             
             update_param = [
@@ -92,7 +100,7 @@ class MyPythonNode(Node):
 
 
 
-        self.timer_publish_imu_values_ = self.create_timer(1.0/self.get_parameter('frequency')._value, self.publish_imu_values)
+        self.timer_publish_imu_values_ = self.create_timer(1.0/self.sample_frequency_, self.publish_imu_values)
 
 
         self.sensorfusion = kalman.Kalman()
@@ -117,7 +125,7 @@ class MyPythonNode(Node):
         self.lastTime = self.get_clock().now()
 
         msg_data.header.stamp = time_stamp
-        msg_data.header.frame_id = self.get_parameter('frame_id')._value
+        msg_data.header.frame_id = self.get_parameter('imu_frame_id')._value
         # Direct measurements
         self.imu.AccelVals = self.imu.OutputTransform.dot(self.imu.AccelVals)
         self.imu.GyroVals = self.imu.OutputTransform.dot(self.imu.GyroVals)
@@ -131,10 +139,10 @@ class MyPythonNode(Node):
         msg_data.angular_velocity.y = (self.imu.GyroVals[1]) 
         msg_data.angular_velocity.z = (self.imu.GyroVals[2])
 
-        if self.get_parameter('pub_raw')._value: 
+        if self.pub_raw_: 
             self.publisher_imu_raw_.publish(msg_data)
 
-        if self.get_parameter('pub_data')._value:        
+        if self.pub_data_:        
             self.sensorfusion.computeAndUpdateRollPitchYaw(\
                 self.imu.AccelVals[0], self.imu.AccelVals[1], self.imu.AccelVals[2],\
                 self.imu.GyroVals[0], self.imu.GyroVals[1], self.imu.GyroVals[2],\
@@ -155,9 +163,9 @@ class MyPythonNode(Node):
             self.publisher_imu_data_.publish(msg_data)
         # print("roll: {:4.2f} \tpitch : {:4.2f} \tyaw : {:4.2f}".format(self.sensorfusion.roll, self.sensorfusion.pitch, self.sensorfusion.yaw))
 
-        if self.get_parameter('pub_mag')._value: 
+        if self.pub_mag_: 
             msg_mag.header.stamp = time_stamp
-            msg_mag.header.frame_id = self.get_parameter('frame_id')._value            
+            msg_mag.header.frame_id = self.get_parameter('imu_frame_id')._value            
             msg_mag.magnetic_field.x = (self.imu.MagVals[0])
             msg_mag.magnetic_field.y = (self.imu.MagVals[1])
             msg_mag.magnetic_field.z = (self.imu.MagVals[2])
